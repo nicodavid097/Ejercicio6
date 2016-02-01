bdaedae3f359b9fbe745c105926fedb7ba62c5bd
Oscar Andres Hernandez Ariza <oahernandez31@misena.edu.co>
2016-02-01 14:24:27 -0500

    Termine Clase Casa

---
+++ Ejercicio6/src/ejercicio6/Casa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio6;
+
+/**
+ *
+ * @author 901540G1
+ */
+public class Casa {
+    private String direccion;
+    
+    public void setDireccion(String direccion){
+    this.direccion=direccion;
+    }
+    
+    public String getDireccion(){
+    return this.direccion;
+    }
+    
+}
