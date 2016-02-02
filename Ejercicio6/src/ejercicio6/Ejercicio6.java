636c909a706e77afc2e0010dd40199ec2e9fcdb1
Peter David Gomez Lobo <pdglobo@misena.edu.co>
2016-02-02 12:31:56 -0500

    commit de la clase ejercicio6

--- Ejercicio6/src/ejercicio6/Ejercicio6.java
+++ Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -15,7 +15,10 @@ public class Ejercicio6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("------PeterD--------");
+        Pc objeto=new Pc();
+        objeto.setMarca("PCmart");
+        System.out.println(objeto.getMarca());
     }
     
 }
